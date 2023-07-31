c731e9514794c0e4321f12f255d09a96b3314ee4
retcon1 <benlucascoding@outlook.com>
2023-07-31 13:00:24 +0100

    feat: changes WaveTransition to accept custom color props. fix: cleans up some scss

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import "./styles/styles.scss";
 import Hello from "./components/Hello";
 import WaveTransition from "./components/WaveTransition";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div>
-      <div className="page-1">
-        <Hello />
-        <WaveTransition />
-      </div>
+      <Hello />
+      <WaveTransition color="#2d3673" />
       <About />
+      <WaveTransition color="#070b22" />
     </div>
   );
 }
