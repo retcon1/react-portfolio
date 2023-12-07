7751352e077c90d27a66b9470bd0fe03e6a9d1ea
retcon1 <benlucascoding@outlook.com>
2023-12-07 17:40:33 +0000

    fix: fixes styling to accomodate for the added portfolio cards

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React　from "react";
+import React from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { Reveal, RightReveal } from "./utils/Reveals";
 import Arrow from "./Arrow";
